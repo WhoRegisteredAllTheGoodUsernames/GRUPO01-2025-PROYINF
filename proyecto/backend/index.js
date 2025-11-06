b5c50279c1d531c52221f92d41b0aa04cd778382
WhoRegisteredAllTheGoodUsernames <38054565+WhoRegisteredAllTheGoodUsernames@users.noreply.github.com>
2025-11-06 02:43:47 -0300

    Mover sesion al iniciador de MiddleWare

--- proyecto/backend/index.js
+++ proyecto/backend/index.js
@@ -2,19 +2,6 @@ const express = require('express');
 const app = express();
 
 const port = 3000;
-//Para tener sesiones de usuario:
-const session = require('express-session');
-app.use(session({
-  secret: 'clave',
-  resave: false,
-  saveUninitialized: true
-}));
-
-
-
-//para poder usar todo tipo de formularios:
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
 const pool = require('./src/db/db'); // Importar la conexión
 const iniciarMiddleware = require('./src/middlewares/iniciarMiddleware')
@@ -23,9 +10,8 @@ const iniciarRutas = require('./src/routes/iniciarRutas')
 
 crearTablas(pool);
 iniciarMiddleware(express, app);
-
-
 iniciarRutas(app);
+
 console.log("HOLA desde index.js");
 
 app.listen(port, () => {
--- proyecto/backend/src/middlewares/iniciarMiddleware.js
+++ proyecto/backend/src/middlewares/iniciarMiddleware.js
@@ -1,6 +1,13 @@
+const session = require('express-session');
+
 function iniciarMiddleware(express, app){
 	app.use(express.json()) // for parsing application/json
 	app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+	app.use(session({
+  		secret: 'clave',
+		resave: false,
+ 		saveUninitialized: true
+	}));
 }
 
 module.exports = iniciarMiddleware;
