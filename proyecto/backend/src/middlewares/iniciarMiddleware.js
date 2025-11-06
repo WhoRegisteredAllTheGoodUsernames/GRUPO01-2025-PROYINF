b0a0d295f94192422a8c1c3697cc8236d216a5c4
WhoRegisteredAllTheGoodUsernames <38054565+WhoRegisteredAllTheGoodUsernames@users.noreply.github.com>
2025-11-06 05:29:46 -0300

    Usar CORS para peticiones del frontend al backend

--- proyecto/backend/src/middlewares/iniciarMiddleware.js
+++ proyecto/backend/src/middlewares/iniciarMiddleware.js
@@ -1,4 +1,5 @@
 const session = require('express-session');
+const cors = require('cors');
 
 function iniciarMiddleware(express, app){
 	app.use(express.json()) // for parsing application/json
@@ -7,6 +8,11 @@ function iniciarMiddleware(express, app){
   		secret: 'clave',
 		resave: false,
  		saveUninitialized: true
+	}));
+	app.use(cors({
+		"origin": "http://localhost:3001",
+		"methods": ["GET", "POST"],
+		"credentials": true
 	}));
 }
 
