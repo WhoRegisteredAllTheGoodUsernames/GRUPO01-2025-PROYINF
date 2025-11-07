4b55bf6a7552e8839fa4f5ccb733cc6c5f03b644
WhoRegisteredAllTheGoodUsernames <38054565+WhoRegisteredAllTheGoodUsernames@users.noreply.github.com>
2025-11-07 00:34:08 -0300

    Crear funcion para obtener datos de la tabla de clientes y mover el sql de usuarios a un archivo general

--- proyecto/backend/src/db/usuarios.js
+++ proyecto/backend/src/db/usuarios.js
@@ -2,6 +2,11 @@ sql = {
 	"crearUser": "INSERT INTO public.user (rut, pass, \"primer-nombre\", \"segundo-nombre\", \"apellido-paterno\", \"apellido-materno\", tipo, \"fecha-nacimiento\") VALUES ($1, $2, $3, $4, $5, $6, $7, $8)",
 	"crearCliente": "INSERT INTO public.cliente (rut) VALUES ($1)",
 	"crearBancario": "INSERT INTO public.bancario (rut, cargo) VALUES ($1, $2)",
+	"obtenerDatosCliente": `
+		SELECT salario, rubro, genero, email, telefono, scoring
+		FROM cliente
+		WHERE rut = $1
+	`,
 };
 
 module.exports = sql;
